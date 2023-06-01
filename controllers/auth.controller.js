1008cfa1c427d52a4cfe057b21b939fc1e0ebc37
egboyguda <e.gboyguda@gmail.com>
2023-06-01 17:56:13 +0800

    added check password if valid  on login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,41 +1,65 @@
-const express = require("express");
 const User = require("../models/user.model");
 const config = require("../config");
-const jwt = require("jwt-simple");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const { Strategy: LocalStrategy } = require("passport-local");
+
+// Configure the passport-local strategy
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+
+      if (!user || !(await user.isValidPassword(password))) {
+        return done(null, false, { message: "Invalid username or password" });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+// Generate JWT token
+function generateToken(user) {
+  const payload = {
+    id: user._id,
+    username: user.username,
+    // Add additional payload data as needed
+  };
+  return jwt.sign(payload, config.jwtSecret, { expiresIn: "7d" });
+}
 
 exports.register = async (req, res) => {
   const { username, password } = req.body;
-  console.log(req.body);
-  try {
-    const user = await new User({ username });
-    await User.register(user, password);
-    const payload = {
-      id: user._id,
-    };
-    const token = jwt.encode(payload, config.jwtSecret);
 
-    res.json({ token: token, userId: user._id, username: user.username });
+  try {
+    const user = new User({ username });
+    await user.setPassword(password);
+    await user.save();
+
+    const token = generateToken(user);
+
+    res.json({ token, userId: user._id, username: user.username });
   } catch (error) {
-    res.status(442).send(error.message);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
-exports.login = async function (req, res) {
-  console.log(req.body);
-  console.log("Logged In");
-  const user = await User.findOne({ username: req.body.username });
-  if (!user) {
-    console.log("Error Happened In auth /token Route");
-  } else {
-    var payload = {
-      id: user.id,
-      expire: Date.now() + 1000 * 60 * 60 * 24 * 7, //7 days
-    };
-    var token = jwt.encode(payload, config.jwtSecret);
-    res.json({
-      token: token,
-      userId: user._id,
-      username: user.username,
-    });
-  }
+exports.login = function (req, res, next) {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: info.message || "Authentication failed" });
+    }
+
+    const token = generateToken(user);
+
+    res.json({ token, userId: user._id, username: user.username });
+  })(req, res, next);
 };
